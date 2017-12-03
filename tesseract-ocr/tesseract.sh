d391a4f69490fcd5100f52b34b322158011f79b6
mrxra <attila.marxer@excessive.li>
2017-12-03 16:20:39 +0100

    fixed pdf overlay

--- tesseract-ocr/tesseract.sh
+++ tesseract-ocr/tesseract.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "[Starting tesseract-ocr service...]"
-mkdir -p /tmp
-inotifywait -e create --monitor --recursive --format '%w%f' "${OCR_DATA}" | while read -r target; do
+OCR_DATA="$1"
+
+ocr_file () {
+    target=$1
     original=/tmp/$(basename ${target})
     echo "${target} -> ${original}"
     cp -p ${target} ${original}
@@ -13,7 +14,17 @@ inotifywait -e create --monitor --recursive --format '%w%f' "${OCR_DATA}" | whil
     echo "tesseract /tmp/${base}.tiff /tmp/${base}.ocr -c tessedit_write_images=true -c textonly_pdf=1 -l deu+eng+fra pdf"
     tesseract /tmp/${base}.tiff /tmp/${base}.ocr -c tessedit_write_images=true -c textonly_pdf=1 -l deu+eng+fra pdf
     echo "pdftk ${original} background /tmp/${base}.ocr.pdf output ${target}"
-    pdftk ${original} background /tmp/${base}.ocr.pdf output ${target}
-done
+    pdftk ${original} multibackground /tmp/${base}.ocr.pdf output ${target}
+}
 
+mkdir -p /tmp
+if [[ -f "$OCR_DATA" ]]; then
+    echo "[processing $OCR_DATA...]"
+    ocr_file "$OCR_DATA"
+else
+    echo "[Starting tesseract-ocr service...]"
+    inotifywait -e create --monitor --recursive --format '%w%f' "${OCR_DATA}" | while read -r target; do
+        ocr_file "$target"
+    done
+fi
 
